76e23e49577984758c44bfde45bc8672d16d0cae
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-03-10 14:29:10 +0500

    Commit

--- src/utils/compress-image.js
+++ src/utils/compress-image.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const Sharp = require('sharp')
 const compressImages = require('compress-images')
 
-const RESIZE_WIDTH = 700;
+let RESIZE_WIDTH = 700;
 const compression = 70;
 
 process.on("message", (payload) => {
@@ -20,32 +20,40 @@ process.on("message", (payload) => {
     };
 
     Sharp(inputPath)
-        .resize({ width: RESIZE_WIDTH })
-        .toFile(tempPath, (err, info) => { 
-            try {
-                if (err) {
-                    endProcess({ statusCode: 409, msg: { error: true, message: "Somethink went wrong", url: null } })
-                }
-                compressImages(tempPath, outputPath, { compress_force: false, statistic: true, autoupdate: true }, false,
-                    { jpg: { engine: "mozjpeg", command: ["-quality", compression] } },
-                    { png: { engine: "pngquant", command: ["--quality=" + compression + "-" + compression, "-o"] } },
-                    { svg: { engine: "svgo", command: "--multipass" } },
-                    { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
-                    async function (error, completed, statistic) {
-                        console.log(error);
-                        console.log(statistic);
-                        if (error) {
-                            throw { statusCode: 500, msg: err?.message || 'Somethink went wrong', url: null }
+        .metadata()
+        .then(metadata => {
+            const { width } = metadata
+            console.log(width);
+            RESIZE_WIDTH = Math.min(RESIZE_WIDTH, width)
+        }).then(() => {
+            Sharp(inputPath)
+                .resize({ width: RESIZE_WIDTH })
+                .toFile(tempPath, (err, info) => {
+                    try {
+                        if (err) {
+                            endProcess({ statusCode: 409, msg: { error: true, message: "Somethink went wrong", url: null } })
                         }
-                        fs.unlink(tempPath, function (error) {
-                            if (error) throw { statusCode: 500, msg: err?.message || 'Somethink went wrong', url: null }
-                        })
-                        endProcess({ statusCode: 201, msg: { error: false, message: 'Image successful compressed and save', url } })
+                        compressImages(tempPath, outputPath, { compress_force: false, statistic: true, autoupdate: true }, false,
+                            { jpg: { engine: "mozjpeg", command: ["-quality", compression] } },
+                            { png: { engine: "pngquant", command: ["--quality=" + compression + "-" + compression, "-o"] } },
+                            { svg: { engine: "svgo", command: "--multipass" } },
+                            { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+                            async function (error, completed, statistic) {
+                                console.log(error);
+                                console.log(statistic);
+                                if (error) {
+                                    throw { statusCode: 500, msg: err?.message || 'Somethink went wrong', url: null }
+                                }
+                                fs.unlink(tempPath, function (error) {
+                                    if (error) throw { statusCode: 500, msg: err?.message || 'Somethink went wrong', url: null }
+                                })
+                                endProcess({ statusCode: 201, msg: { error: false, message: 'Image successful compressed and save', url } })
+                            }
+                        )
+                    } catch (e) {
+                        endProcess({ statusCode: 409, msg: { error: true, message: "Somethink went wrong", url: null } })
                     }
-                )
-            } catch (e) {
-                endProcess({ statusCode: 409, msg: { error: true, message: "Somethink went wrong", url: null } })
-            }
 
+                })
         })
 });
